db65fc948c4f282dc533ec90ed9c9bc99968e84c
Elliot Park <elliotpark410@gmail.com>
2023-02-12 14:04:25 +0700

    update portfolio with latest experience

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,7 +6,7 @@ export default class Porfolio extends Component {
       <section id="portfolio">
       <div className="row">
         <div className="twelve columns collapsed">
-          <h1>Check Out Some of My Works.</h1>
+          <h1>Check Out Some of My Work</h1>
           <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
           {
             resumeData.portfolio && resumeData.portfolio.map((item)=>{
@@ -15,13 +15,13 @@ export default class Porfolio extends Component {
                   <div className="item-wrap">
                     <a href="#modal-01">
                       <img src={`${item.imgurl}`}className="item-img"/>
-                      
+
                       <div className="overlay">
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
                           <p>{item.description}
                           </p>
-                          
+
                         </div>
                       </div>
                     </a>
@@ -36,7 +36,7 @@ export default class Porfolio extends Component {
                     </p>
                   </div>
 
-                  
+
 
                 </div>
               )
--- src/resumeData.js
+++ src/resumeData.js
@@ -3,7 +3,7 @@ let resumeData = {
     "role": "Backend Developer",
     "linkedinId":"https://www.linkedin.com/in/elliot-park/",
     "email": "elliotpark410@gmail.com",
-    "roleDescription": "I like building things and solving problems. Currently living in Bali, Indonesia.",
+    "roleDescription": "I'm a junior back end developer based in Bali, Indonesia. Currently, I'm focused on building a decentralized, blockchain native payment system with Jagad.",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -21,7 +21,7 @@ let resumeData = {
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"My journey into coding began with University of California, Berkeley's Full Stack Web Development bootcamp. There were a lot of sleepless nights, but it was worth every second. After I deployed my first app, I was hooked and I knew this is what I want to do for the rest of my life. The bootcamp taught me how to problem solve, work with a team, and learn new things every day. I am currently doing operational work for a crypto-finance company in Bali. When I am not working I use my spare time to sharpen my skills in React and JavaScript. If I am not in front of my computer you can find me surfing at the beach or doing jiu jitsu on the mats. Thanks for checking out my portfolio, and feel free to reach out if you have any questions!",
+    "aboutme":"My journey into coding began with University of California, Berkeley's Full Stack Web Development bootcamp. There were a lot of sleepless nights, but it was worth every second. After I deployed my first app, I was hooked and I knew this is what I wanted to do for the rest of my life. Currently, I am helping build the backend for Jagad which provides a decentralized stablecoin pegged to the Indonesian rupiah. I grew up in Southern California, lived in the Bay Area for a few years, and I am living in Bali, Indonesia. If I am not in front of my computer you can find me surfing at the beach or doing jiu jitsu on the mats. Please don't hesitate to reach out, and thank you for checking out my portfolio!",
     "address":"Bali, Indonesia",
     "website":"E: elliotpark410@gmail.com",
     "education":[
@@ -30,35 +30,40 @@ let resumeData = {
         "specialization":"Philosophy",
         "MonthOfPassing":"Jun",
         "YearOfPassing":"2016",
-        "Achievements":"Additional Coursework: University of California, Berkeley Full Stack Web Development bootcamp"
+      },
+      {
+        "UniversityName":"University of California, Berkeley",
+        "specialization":"Full Stack Web Development Bootcamp",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2022",
       }
     ],
     "work":[
       {
-        "CompanyName":"Systematic Trading Group",
-        "specialization":"Operations Team Lead",
+        "CompanyName":"Jagad",
+        "specialization":"Junior Back End Developer",
         "YearOfLeaving":"Present",
-        "Achievements": "Translate strategy into actionable goals to execute cryptocurrency arbitrage within different markets in South Korea and USA. Adhere to company, federal, state, and local business requirements, enforcing compliance and taking action when necessary. Monitor performance with tracking and establish corrective measures as needed, and prepare detailed reports. Define project requirements and timeline, and organize team to meet business needs within budget. Conduct recurring check-in meetings with C-suite to analyze business strategy and deliver reports. Collaborate with other engineers to whiteboard, pseudocode, debug, and review code."
+        "Achievements": "Make Bash script to convert Postman docs from json to yaml and preview with ReDoc Docker image. Create unit tests and integration tests with Jest and incorporate mock functions when necessary. Run CI/CD jobs with Github Actions and review new commits to debug, refactor, or update tests. Schedule cron job and connect to Fireblocks and Slack API to send daily updates of asset data. Build and debug API endpoints with TypeScript, Node, Express, Prisma, Postman, and MySQL. Log API data with custom logger and configure pm2-logrotate in dev and staging server"
       },
       {
         "CompanyName":"SingleStore",
         "specialization":"Account Executive",
         "MonthOfLeaving":"Oct",
         "YearOfLeaving":"2021",
-        "Achievements":"Build lasting customer relationships with stakeholders by providing solutions to business challenges. Generate new business while expanding footprint into existing accounts with high outbound activity. Practice clear communication of product by thoroughly understanding technical capabilities. Create detailed usage reports to analyze usage and positively influence renewal business. Manage renewal and upsell business with quota attainment, achieved 100% renewal rate. Conduct online training sessions and best practice webinars to ensure customer success."
+        "Achievements":"Generated new business while expanding footprint into existing accounts with high outbound activity. Built lasting customer relationships with stakeholders by providing solutions to business challenges. Practiced clear communication of product by thoroughly understanding technical capabilities. Conducted online training sessions and best practice webinars to ensure customer success. Created detailed usage reports to analyze usage and positively influence renewal business"
       },
       {
         "CompanyName":"Walpole & Co., LLP",
         "specialization":"Tax Consultant",
         "MonthOfLeaving":"Jun",
         "YearOfLeaving":"2019",
-        "Achievements": "Retrieved client data and analyzed various accounting software with the goal of improving processes. Generated custom analytics and reports to detect issues with user adoption and inventory processing. Conducted training sessions to prepare more than two hundred employees for new software adoption. Extracted client data from numerous sources and prepared financial reports for inventory accuracy. Provided tax consultation with clients in tech, manufacturing, financial, and real estate industries. Implemented SaaS software to Tier 1 global companies and provided technical support."
+        "Achievements": "Provided tax consultation with clients in tech, manufacturing, financial, and real estate industries. Conducted training sessions to prepare a couple hundred employees for new software adoption. Implemented SaaS accounting software and improved inventory processing with analytics. Utilized professional software such as Lacerte, Quickbooks, and Microsoft Excel"
       }
     ],
-    "skillsDescription":"HTML, CSS, JavaScript, TypeScript, React, MySQL, MongoDB, Express, Node, Jest",
+    "skillsDescription":"Git, Bash, HTML, CSS, React, JavaScript, Jest, TypeScript, SQL, Node, Express, Prisma, Sequelize, Mongoose, MySQL, MongoDB, Postman, Insomnia, Docker, Linux, and OOP",
     "skills":[
       {
-        "skillname":"Express"
+        "skillname":"Node"
       },
       {
         "skillname":"MySQL"
@@ -83,7 +88,7 @@ let resumeData = {
         "description":"A CMS-style blog site similar to Wordpress.",
         "imgurl":"images/portfolio/tech-blog.png",
         "imgrepo": "https://github.com/elliotpark410/Tech-Blog",
-        "imglink": "https://desolate-atoll-11549.herokuapp.com/"
+        "imglink": "https://ep-tech-blog.herokuapp.com/"
       },
       {
         "name":"Team Profile Generator",
@@ -97,7 +102,7 @@ let resumeData = {
         "description":"A text editor app that runs in the browser and meets the Progressive Web App criteria.",
         "imgurl":"images/portfolio/text-editor.png",
         "imgrepo": "https://github.com/elliotpark410/Text-Editor",
-        "imglink": "https://pure-chamber-15361.herokuapp.com/"
+        "imglink": "https://drive.google.com/file/d/1JcmvHDmYU6WjGITv-FaK7d9HGSF34AQG/view"
       }
     ]
   }
