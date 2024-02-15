d58212ee383162c38bbd24d9217d2d794d6a786f
Elliot Park <elliotpark410@gmail.com>
2024-02-14 17:54:08 -0800

    update resume experience

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,86 +1,92 @@
 let resumeData = {
-    "name": "Elliot Park",
-    "role": "Full Stack Web Developer",
-    "linkedinId":"https://www.linkedin.com/in/elliot-park/",
-    "email": "elliotpark410@gmail.com",
-    "roleDescription": "I am here to infiltrate earthling technology with Full Stack Web Development.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/elliot-park/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://github.com/elliotpark410/",
-          "className":"fa fa-github"
-        }
-      ],
-    "aboutme":"My journey into coding began with the Full Stack Web Development bootcamp at the University of California, Berkeley. Despite the sleepless nights, every moment was worth it. The thrill of deploying my first app ignited a passion within me, solidifying my decision to pursue coding as a lifelong career. I thrive on learning about emerging technologies and relish the challenge of solving complex problems. With nearly two years of experience as a Full Stack Developer, I've had the opportunity to contribute to a wide range of projects, including the design of customer-facing websites and the development of RESTful API endpoints for a fintech mobile app. One of my proudest achievements has been successfully integrating the server with third-party APIs to implement essential features such as transaction confirmation, P2P transfers, and push notifications. At Jagad, where I currently work as a Node Developer, I've honed my skills in building secure and scalable API endpoints that adhere to strict security standards. I've collaborated with cross-functional teams to identify partnership requirements and develop open API features for payment gateway capability. When I'm not immersed in code, you can find me riding the waves at the beach or practicing jiu jitsu on the mats. I thrive on new challenges and am always eager to expand my horizons. Don't hesitate to reach out — I'm excited to connect and share more about my journey in tech!",
-    "address":"Irvine, California",
-    "website":"E: elliotpark410@gmail.com",
-    "education":[
-      {
-        "UniversityName":"University of California, Berkeley",
-        "specialization":"Full Stack Web Development Bootcamp",
-        "MonthOfPassing":"Mar",
-        "YearOfPassing":"2022",
-      },
-      {
-        "UniversityName":"University of California, Santa Barbara",
-        "specialization":"Philosophy",
-        "MonthOfPassing":"Jun",
-        "YearOfPassing":"2016",
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Jagad",
-        "specialization":"Full Stack Developer",
-        "MonthOfLeaving":"Feb",
-        "YearOfLeaving":"2024",
-        "Achievements": `
+  name: 'Elliot Park',
+  role: 'Full Stack Web Developer',
+  linkedinId: 'https://www.linkedin.com/in/elliot-park/',
+  email: 'elliotpark410@gmail.com',
+  roleDescription:
+    'I am here to infiltrate earthling technology with Full Stack Web Development.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/elliot-park/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/elliotpark410/',
+      className: 'fa fa-github',
+    },
+  ],
+  aboutme:
+    "My journey into coding began with the Full Stack Web Development bootcamp at the University of California, Berkeley. Despite the sleepless nights, every moment was worth it. The thrill of deploying my first app ignited a passion within me, solidifying my decision to pursue coding as a lifelong career. I thrive on learning about emerging technologies and relish the challenge of solving complex problems. With nearly two years of experience as a Full Stack Developer, I've had the opportunity to contribute to a wide range of projects, including the design of customer-facing websites and the development of RESTful API endpoints for a fintech mobile app. One of my proudest achievements has been successfully integrating the server with third-party APIs to implement essential features such as transaction confirmation, P2P transfers, and push notifications. At Jagad, where I currently work as a Node Developer, I've honed my skills in building secure and scalable API endpoints that adhere to strict security standards. I've collaborated with cross-functional teams to identify partnership requirements and develop open API features for payment gateway capability. When I'm not immersed in code, you can find me riding the waves at the beach or practicing jiu jitsu on the mats. I thrive on new challenges and am always eager to expand my horizons. Don't hesitate to reach out — I'm excited to connect and share more about my journey in tech!",
+  address: 'Irvine, California',
+  website: 'E: elliotpark410@gmail.com',
+  education: [
+    {
+      UniversityName: 'University of California, Berkeley',
+      specialization: 'Full Stack Web Development Bootcamp',
+      MonthOfPassing: 'Mar',
+      YearOfPassing: '2022',
+    },
+    {
+      UniversityName: 'University of California, Santa Barbara',
+      specialization: 'Philosophy',
+      MonthOfPassing: 'Jun',
+      YearOfPassing: '2016',
+    },
+  ],
+  work: [
+    {
+      CompanyName: 'Jagad',
+      specialization: 'Node Developer',
+      // "MonthOfLeaving":"",
+      YearOfLeaving: 'Currently Employed',
+      Achievements: `
         • Design and debug RESTful API endpoints for a fintech mobile application that meets strict security standards, ensuring the protection of sensitive user data
-        • Develop hundreds unit tests, integration tests, and end-to-end tests to verify the functionality of back-end processing, database interactions, and business logic
+        • Develop hundreds of unit tests, integration tests, and end-to-end tests to verify the functionality of back-end processing, database interactions, and business logic
         • Coordinate with other engineering teams to identify partnership requirements, create documentation, and build open API features for payment gateway capability
-        • Create interoperability for Telegram bot to interact with backend API endpoints, allowing users to redeem to bank and redeem to e-wallet.
-        • Improve customer experience by integrating server with third-party APIs like Fireblocks, DurianPay, Solana, ICP, and Slack
+        • Create interoperability for Telegram bot to interact with backend API endpoints, allowing users to redeem to bank and redeem to e-wallet
+        • Improve customer experience by integrating the server with third-party APIs like Fireblocks, DurianPay, Solana, ICP, and Slack
         • Refactor database schema to adhere to database normalization design principles, enhancing performance
         • Collaborate with a team of 8 developers to build diagrams and workflows for new feature specifications
-        `
-      },
-    ],
-    "skillsDescription":"Git, Bash, HTML, CSS, React, JavaScript, Jest, TypeScript, SQL, Node, Express, Prisma, Sequelize, Mongoose, MySQL, MongoDB, Postman, Insomnia, Docker, Linux, Sentry, CI/CD",
-    "portfolio":[
-      {
-        "name":"Code Quiz",
-        "description":"A simple code quiz that allows users to test their JavaScript knowledge.",
-        "imgurl":"images/portfolio/code-quiz.png",
-        "imgrepo": "https://github.com/elliotpark410/Code-Quiz",
-        "imglink": "https://elliotpark410.github.io/Code-Quiz/"
-      },
-      {
-        "name":"Tech Blog",
-        "description":"A CMS-style blog site similar to Wordpress.",
-        "imgurl":"images/portfolio/tech-blog.png",
-        "imgrepo": "https://github.com/elliotpark410/Tech-Blog",
-        "imglink": "https://ep-tech-blog.herokuapp.com/"
-      },
-      {
-        "name":"Team Profile Generator",
-        "description":"A command-line application that generates a team profile.",
-        "imgurl":"images/portfolio/team-profile.png",
-        "imgrepo": "https://github.com/elliotpark410/Team-Profile-Generator",
-        "imglink": "https://elliotpark410.github.io/Team-Profile-Generator/"
-      },
-      {
-        "name":"Text Editor",
-        "description":"A text editor app that runs in the browser and meets the Progressive Web App criteria.",
-        "imgurl":"images/portfolio/text-editor.png",
-        "imgrepo": "https://github.com/elliotpark410/Text-Editor",
-        "imglink": "https://drive.google.com/file/d/1JcmvHDmYU6WjGITv-FaK7d9HGSF34AQG/view"
-      }
-    ]
-  }
+        `,
+    },
+  ],
+  skillsDescription:
+    'Git, Bash, HTML, CSS, React, JavaScript, Jest, TypeScript, SQL, Node, Express, Prisma, Sequelize, Mongoose, MySQL, MongoDB, Postman, Insomnia, Docker, Linux, Sentry, CI/CD',
+  portfolio: [
+    {
+      name: 'Code Quiz',
+      description:
+        'A simple code quiz that allows users to test their JavaScript knowledge.',
+      imgurl: 'images/portfolio/code-quiz.png',
+      imgrepo: 'https://github.com/elliotpark410/Code-Quiz',
+      imglink: 'https://elliotpark410.github.io/Code-Quiz/',
+    },
+    {
+      name: 'Tech Blog',
+      description: 'A CMS-style blog site similar to Wordpress.',
+      imgurl: 'images/portfolio/tech-blog.png',
+      imgrepo: 'https://github.com/elliotpark410/Tech-Blog',
+      imglink: 'https://ep-tech-blog.herokuapp.com/',
+    },
+    {
+      name: 'Team Profile Generator',
+      description: 'A command-line application that generates a team profile.',
+      imgurl: 'images/portfolio/team-profile.png',
+      imgrepo: 'https://github.com/elliotpark410/Team-Profile-Generator',
+      imglink: 'https://elliotpark410.github.io/Team-Profile-Generator/',
+    },
+    {
+      name: 'Text Editor',
+      description:
+        'A text editor app that runs in the browser and meets the Progressive Web App criteria.',
+      imgurl: 'images/portfolio/text-editor.png',
+      imgrepo: 'https://github.com/elliotpark410/Text-Editor',
+      imglink:
+        'https://drive.google.com/file/d/1JcmvHDmYU6WjGITv-FaK7d9HGSF34AQG/view',
+    },
+  ],
+};
 
-  export default resumeData
+export default resumeData;
